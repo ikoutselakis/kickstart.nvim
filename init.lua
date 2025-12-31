1f97dbc096140482ee442c98ff7351b7fca14775
ikoutselakis <ikoutselakis@gmail.com>
2025-12-31 23:46:19 +0200

    added pyright as lsp

--- init.lua
+++ init.lua
@@ -587,7 +587,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        --pyright = {},
+        pyright = {},
         ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
